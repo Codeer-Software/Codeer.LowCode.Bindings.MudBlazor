a14c9ab19cea9f12f205b858e3a7cb2dd473af45
yutaka <tsumori-yutaka@codeer.co.jp>
2024-09-18 16:43:31 +0900

    FileFieldの見た目を修正

--- Codeer.LowCode.Bindings.MudBlazor/Designs/MudFileFieldDesign.cs
+++ Codeer.LowCode.Bindings.MudBlazor/Designs/MudFileFieldDesign.cs
@@ -1,11 +1,18 @@
 ﻿using Codeer.LowCode.Bindings.MudBlazor.Components;
+using Codeer.LowCode.Bindings.MudBlazor.Enums;
 using Codeer.LowCode.Blazor.Repository.Design;
 
 namespace Codeer.LowCode.Bindings.MudBlazor.Designs
 {
     [DisableBulkDataUpdate]
     public class MudFileFieldDesign : FileFieldDesign
-    {
+    {   
+        [Designer]
+        public MudVariant Variant { get; set; } = MudVariant.Filled;
+
+        [Designer]
+        public MudColor Color { get; set; } = MudColor.Default;
+        
         public MudFileFieldDesign() => TypeFullName = typeof(MudFileFieldDesign).FullName!;
         public override string GetWebComponentTypeFullName() => typeof(MudFileFieldComponent).FullName!;
     }
